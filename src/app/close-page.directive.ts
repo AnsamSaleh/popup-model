2e849763940ff25c7d644016fefff308e6b4dfda
Ansam <ansam_saleh_95@hotmail.com>
2018-08-07 13:24:52 +0300

    Finished Popup Modal

--- src/app/close-page.directive.ts
+++ src/app/close-page.directive.ts
@@ -1,14 +1,20 @@
-import {Directive, ElementRef, HostBinding, HostListener, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, OnInit, OnDestroy, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appClosePage]'
 })
-export class ClosePageDirective {
+export class ClosePageDirective implements OnInit, OnDestroy {
  // @HostBinding ('class.visible') private  isshow: boolean;
  // @HostBinding('scrollTop') private scrollTop : number = 100;
 
   constructor(private el: ElementRef , private renderer: Renderer2) {
     //this.renderer.setStyle(this.el,'display', 'none');
+  }
+  ngOnInit() {
+    document.body.style.overflow = 'hidden';
+  }
+  ngOnDestroy() {
+    document.body.style.overflow = '';
   }
   @HostListener('click') onClick() {
    // console.log("close");
